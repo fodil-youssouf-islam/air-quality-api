89eb0df212310caf7a1b56e3d75d24078118e63c
fodil-youssouf-islam <fodilyoussoufislam@gmail.com>
2022-11-26 11:07:28 +0100

    add e2e tests.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,48 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
+import { Test } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
 import { AppModule } from "../src/app.module";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.setGlobalPrefix("api");
     await app.init();
   });
 
-  it("/", () => {
+  it(`Returns Hello World Message`, () => {
     return request(app.getHttpServer())
-      .get("/")
+      .get("/api/")
       .expect(200)
       .expect("Hello World!");
   });
+
+  it(`Returns an air quality json object`, () => {
+    return request(app.getHttpServer())
+      .get("/api/air-quality")
+      .expect(200);
+  });
+
+  it(`Returns an air quality json object`, () => {
+    return request(app.getHttpServer())
+      .get("/api/air-quality?longitude=0.744414&latitude=7.44415852")
+      .expect(200);
+  });
+
+  it(`Returns the most polluted air quality json object stored locally`, () => {
+    return request(app.getHttpServer())
+      .get("/api/most-polluted")
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
 });
+
